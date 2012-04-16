a41cf21dd74fb749ff1beba7aafb65de240ccfdb
Pavel Frolov <tigrashik@gmail.com>
2012-04-16 08:29:21 +0400

    Попытка сделать из кода менялки обоев нечто понятное и читаемое.

--- wpchanger/unMain.dfm
+++ wpchanger/unMain.dfm
@@ -1,10 +1,10 @@
 object fmMain: TfmMain
   Left = 219
   Top = 154
-  Width = 237
-  Height = 152
   BorderIcons = [biSystemMenu, biMinimize]
   Caption = #1052#1077#1085#1103#1083#1082#1072' '#1086#1073#1086#1077#1074
+  ClientHeight = 118
+  ClientWidth = 229
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -14,14 +14,14 @@ object fmMain: TfmMain
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
-  object Button2: TButton
+  object btChange: TButton
     Left = 8
     Top = 8
     Width = 209
     Height = 49
     Caption = #1057#1084#1077#1085#1080#1090#1100
     TabOrder = 0
-    OnClick = Button2Click
+    OnClick = btChangeClick
   end
   object RadioGroup1: TRadioGroup
     Left = 16
@@ -31,7 +31,7 @@ object fmMain: TfmMain
     Caption = #1060#1086#1088#1084#1072#1090
     TabOrder = 1
   end
-  object RadioButton1: TRadioButton
+  object rbBMP: TRadioButton
     Left = 88
     Top = 80
     Width = 49
@@ -39,7 +39,7 @@ object fmMain: TfmMain
     Caption = 'BMP'
     TabOrder = 2
   end
-  object RadioButton2: TRadioButton
+  object rbJPEG: TRadioButton
     Left = 32
     Top = 80
     Width = 57
@@ -49,7 +49,7 @@ object fmMain: TfmMain
     TabOrder = 3
     TabStop = True
   end
-  object RadioButton3: TRadioButton
+  object rbPNG: TRadioButton
     Left = 144
     Top = 80
     Width = 57
@@ -58,11 +58,11 @@ object fmMain: TfmMain
     TabOrder = 4
   end
   object XPManifest1: TXPManifest
-    Left = 40
-    Top = 8
+    Left = 120
+    Top = 24
   end
-  object OpenPictureDialog1: TOpenPictureDialog
-    Left = 8
-    Top = 8
+  object opdImage: TOpenPictureDialog
+    Left = 24
+    Top = 24
   end
 end
--- wpchanger/unMain.pas
+++ wpchanger/unMain.pas
@@ -9,18 +9,18 @@ uses
 
 type
   TfmMain = class(TForm)
-    Button2: TButton;
+    btChange: TButton;
     XPManifest1: TXPManifest;
     RadioGroup1: TRadioGroup;
-    RadioButton1: TRadioButton;
-    RadioButton2: TRadioButton;
-    RadioButton3: TRadioButton;
-    OpenPictureDialog1: TOpenPictureDialog;
-    procedure Button2Click(Sender: TObject);
-    function jpegtobmp(const filename: tfilename):ANSIstring;
-    function convert(const filename3: tfilename):ANSIstring;
-    function pngtobmp(const filename1: tfilename):ANSIstring;
-    function bmptobmp(const filename2: tfilename):ANSIstring;
+    rbBMP: TRadioButton;
+    rbJPEG: TRadioButton;
+    rbPNG: TRadioButton;
+    opdImage: TOpenPictureDialog;
+    procedure btChangeClick(Sender: TObject);
+    function jpegtobmp(const filename: tfilename): ANSIstring;
+    function convert(const filename3: tfilename): ANSIstring;
+    function pngtobmp(const filename1: tfilename): ANSIstring;
+    function bmptobmp(const filename2: tfilename): ANSIstring;
   private
     { Private declarations }
   public
@@ -34,87 +34,108 @@ implementation
 
 {$R *.dfm}
 
-function TfmMain.bmptobmp(const filename2: tfilename):ANSIstring;
-  var
-    Bitmap: TBitmap;
-    name:ANSIstring;
-  begin
-    Bitmap := TBitmap.Create;
-    try
-    Bitmap.LoadFromFile(filename2);
-    name:=GetEnvironmentVariable('APPDATA')+'\'+'wallpaper.bmp';
-    Bitmap.SaveToFile(name);
-    result:=name;
- finally
-   Bitmap.Free;
- end
-end;
-
-function TfmMain.pngtobmp(const filename1:tfilename):ANSIstring;
+// "Преобразование" BMP в BMP
+function TfmMain.bmptobmp(const filename2: tfilename): ANSIstring;
 var
- Bitmap: TBitmap;
- PNG: TPNGObject;
- name:ANSIstring;
+  Bitmap: TBitmap;
+  name: ANSIstring;
 begin
- PNG := TPNGObject.Create;
- Bitmap := TBitmap.Create;
- {In case something goes wrong, free booth PNG and Bitmap}
- try
-   PNG.LoadFromFile(filename1);
-   Bitmap.Assign(PNG);    //Convert data into bitmap
-   name:=GetEnvironmentVariable('APPDATA')+'\'+'wallpaper.bmp';
-   Bitmap.SaveToFile(name);
-   result:=name;
- finally
-   PNG.Free;
-   Bitmap.Free;
- end
+  Bitmap := TBitmap.Create;
+  try
+    Bitmap.LoadFromFile(filename2);
+    name := GetEnvironmentVariable('APPDATA') + '\' + 'wallpaper.bmp';
+    Bitmap.SaveToFile(name);
+    result := name;
+  finally
+    Bitmap.Free;
+  end
 end;
 
-function TfmMain.convert(const filename3: tfilename):ANSIstring;
+// Преобразование PNG в BMP
+function TfmMain.pngtobmp(const filename1: tfilename): ANSIstring;
+var
+  Bitmap: TBitmap;
+  PNG: TPNGObject;
+  name: ANSIstring;
 begin
-  if Radiobutton2.Checked then Result:=jpegtobmp(filename3);
-  if Radiobutton1.Checked then Result:=bmptobmp(filename3);
-  if Radiobutton3.Checked then Result:=pngtobmp(filename3);
+  PNG := TPNGObject.Create;
+  Bitmap := TBitmap.Create;
+  { In case something goes wrong, free booth PNG and Bitmap }
+  try
+    PNG.LoadFromFile(filename1);
+    Bitmap.Assign(PNG); // Convert data into bitmap
+    name := GetEnvironmentVariable('APPDATA') + '\' + 'wallpaper.bmp';
+    Bitmap.SaveToFile(name);
+    result := name;
+  finally
+    PNG.Free;
+    Bitmap.Free;
+  end
 end;
 
-function TfmMain.jpegtobmp(const filename: tfilename):ANSIstring;
+// Выбор функции для преобразования
+function TfmMain.convert(const filename3: tfilename): ANSIstring;
+begin
+  if rbJPEG.Checked then
+    result := jpegtobmp(filename3);
+  if rbBMP.Checked then
+    result := bmptobmp(filename3);
+  if rbPNG.Checked then
+    result := pngtobmp(filename3);
+end;
+
+// Преобразование JPEG в BMP
+function TfmMain.jpegtobmp(const filename: tfilename): ANSIstring;
 var
   jpeg: tjpegimage;
-  bmp: tbitmap;
-  name:ANSIstring;
+  bmp: TBitmap;
+  name: ANSIstring;
 begin
-  jpeg := tjpegimage.create;
-try
-  jpeg.compressionquality := 100; {default value}
-  jpeg.loadfromfile(filename);
-  bmp := tbitmap.create;
-try
-  bmp.assign(jpeg);
-  name:=GetEnvironmentVariable('APPDATA')+'\'+'wallpaper.bmp';
-  bmp.savetofile(name);
-  result:=name;
-finally
-  bmp.free
-end;
-finally
-  jpeg.free
-end;
+  jpeg := tjpegimage.Create;
+  try
+    jpeg.compressionquality := 100; { default value }
+    jpeg.LoadFromFile(filename);
+    bmp := TBitmap.Create;
+    try
+      bmp.Assign(jpeg);
+      name := GetEnvironmentVariable('APPDATA') + '\' + 'wallpaper.bmp';
+      bmp.SaveToFile(name);
+      result := name;
+    finally
+      bmp.Free
+    end;
+  finally
+    jpeg.Free
+  end;
 end;
 
-procedure TfmMain.Button2Click(Sender: TObject);
-//const
-//  CLSID_ActiveDesktop: TGUID = '{75048700-EF1F-11D0-9888-006097DEACF9}';
-//  ActiveDesktop: IActiveDesktop;
+// Выбор файла, преобразование и установка обоев
+procedure TfmMain.btChangeClick(Sender: TObject);
+// Хитрый способ для установки обоев без преобразование
+// const
+// CLSID_ActiveDesktop: TGUID = '{75048700-EF1F-11D0-9888-006097DEACF9}';
+// ActiveDesktop: IActiveDesktop;
 begin
-//  ActiveDesktop := CreateComObject(CLSID_ActiveDesktop) as IActiveDesktop;
-//  ActiveDesktop.SetWallpaper(pwchar(Edit1.Text), 0);
-//  ActiveDesktop.ApplyChanges(AD_APPLY_ALL or AD_APPLY_FORCE);
-  if Radiobutton2.Checked then OpenPictureDialog1.Filter:='jpeg|*.jpeg; *.jpg';
-  if Radiobutton1.Checked then OpenPictureDialog1.Filter:='bmp|*.bmp';
-  if Radiobutton3.Checked then OpenPictureDialog1.Filter:='png|*.png';
-  if OpenPictureDialog1.Execute then
-  if SystemParametersInfo(SPI_SETDESKWALLPAPER,0,pchar(convert(OpenPictureDialog1.FileName)),SPIF_UPDATEINIFILE) then ShowMessage('Сделано!')
-    else Showmessage('Неудача!');
+  // ActiveDesktop := CreateComObject(CLSID_ActiveDesktop) as IActiveDesktop;
+  // ActiveDesktop.SetWallpaper(pwchar(Edit1.Text), 0);
+  // ActiveDesktop.ApplyChanges(AD_APPLY_ALL or AD_APPLY_FORCE);
+
+  // Настройка фильтра
+  if rbJPEG.Checked then
+    opdImage.Filter := 'jpeg|*.jpeg; *.jpg';
+  if rbBMP.Checked then
+    opdImage.Filter := 'bmp|*.bmp';
+  if rbPNG.Checked then
+    opdImage.Filter := 'png|*.png';
+  // Преобразование
+  if opdImage.Execute then
+    if SystemParametersInfo(SPI_SETDESKWALLPAPER, 0,
+      pchar(convert(opdImage.filename)), SPIF_UPDATEINIFILE) then
+      MessageBox(Self.Handle, 'Обои изменены.', 'Информация',
+        MB_OK or MB_ICONINFORMATION)
+    else
+      MessageBox(Self.Handle, 'Обои не изменены!', 'Ошибка!',
+        MB_OK or MB_ICONERROR);
 end;
+
 end.
