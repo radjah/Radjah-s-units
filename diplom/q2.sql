767dedba09166c415a2a0c3c9f149b60d5cc9335
Pavel Frolov <tigrashik@gmail.com>
2014-08-28 20:33:22 +0400

    Перенес некоторые проекты в другой репозиторий.

--- diplom/q2.sql
+++
@@ -1,4 +0,0 @@
-select id_place,id_sig, count(id_sig) as sigcount
-from connector conn inner join contact cnt on conn.id = cnt.id_conn
-group by id_place,id_sig
-order by id_place,id_sig
--- diplom/q3.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE ##contact(
-	id_place int NOT NULL,
-	id_sig int NOT NULL,
-	sigcount int NOT NULL)
